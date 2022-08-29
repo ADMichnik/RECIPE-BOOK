bc91f5dde16ca2e55ecd52cc7dab02ddfad649fd
ADMichnik <amek995@gmail.com>
2022-08-29 14:36:46 +0200

    initial commit

---
+++ src/app/header-app/header-app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header-app.component.html',
+  styleUrls: ['./header-app.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
